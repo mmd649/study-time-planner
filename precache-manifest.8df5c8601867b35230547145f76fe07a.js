2366394c19fee00f7457899bdc1d8231c8b9d2c0
Mark <mmed9649@gmail.com>
2020-10-05 15:07:51 +0100

    Updates

---
+++ precache-manifest.8df5c8601867b35230547145f76fe07a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc6aaf8357842cc53296d78c1538caa",
+    "url": "/study-time-planner/index.html"
+  },
+  {
+    "revision": "83bf0612b89b9ac0a53b",
+    "url": "/study-time-planner/static/css/main.556156ed.chunk.css"
+  },
+  {
+    "revision": "154e4339196997fb53bc",
+    "url": "/study-time-planner/static/js/2.404e8c3e.chunk.js"
+  },
+  {
+    "revision": "62c1645b53df7f873fca458c71c36e8c",
+    "url": "/study-time-planner/static/js/2.404e8c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bf0612b89b9ac0a53b",
+    "url": "/study-time-planner/static/js/main.0c674acf.chunk.js"
+  },
+  {
+    "revision": "62e9add1d6414fc3fcc3",
+    "url": "/study-time-planner/static/js/runtime-main.9119ab89.js"
+  },
+  {
+    "revision": "2f154994f9c43e0768a5517b3a9c50f6",
+    "url": "/study-time-planner/static/media/alarm.2f154994.mp3"
+  }
+]);
