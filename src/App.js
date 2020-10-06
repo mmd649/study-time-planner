f31a09392dcd3f5cc5dedf9267ba71521f4fc2af
Mark <mmed9649@gmail.com>
2020-10-06 20:45:24 +0100

    fixed routing for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/navbar';
 import Timer from './components/timer'
@@ -8,7 +8,7 @@ import About from './components/about'
 
 function App() {
   return (
-    <Router>
+    <Router basename='/'>
       <div>
         <Navbar />
         <Switch>
